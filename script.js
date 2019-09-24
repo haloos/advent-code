94befd5e6d72398032a992bd55c3538ee9bb2483
Jason Paredes <haloos@me.com>
2019-09-23 20:59:38 -0400

    created function

--- script.js
+++ script.js
@@ -4,7 +4,8 @@ const fs = require('fs');
 // ) --> should go DOWN 1 floor 
 
 function question1() {
-  fs.readFile('./santa.text', (err, data) => {
+  fs.readFile('./santa.text', (err, data) => { 
+    console.time('santa-time');
     const directions = data.toString(); 
     const directionsArray = directions.split(''); 
     const answer = directionsArray.reduce((acc, currentValue) =>{
@@ -13,7 +14,8 @@ function question1() {
       } else if (currentValue === ')') {
         return acc -= 1
       }
-    }, 0)
+    }, 0) 
+    console.timeEnd('santa-time');
     console.log('floor:', answer);
   })
 }  
