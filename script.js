7c4a95cf7377f763692df319512e7846b2d6aeb8
Jason Paredes <haloos@me.com>
2019-09-23 21:19:35 -0400

    added currentItem

--- script.js
+++ script.js
@@ -20,25 +20,33 @@ function question1() {
   })
 }  
 
-question1() 
+// question1() 
 
 
 
 // 2 - when does Santa first enter the basement 
 function question2() {
- fs.readFile('./sana.txt', (err, data) => {
+ fs.readFile('./santa.txt', (err, data) => { 
+  console.time('santa-time');
   const directions = data.toString(); 
   const directionsArray = directions.split(''); 
   let accumulator = 0
-  const answer = directionsArray.some((currentValue) => {
-    if (currentValue === '(') {
-      return accumulator += 1
+  let counter = 0
+  const answer = directionsArray.some((currentItem) => {
+    if (currentItem === '(') {
+       accumulator += 1
     } else if (currentValue === ')') {
-      return accumulator -= 1
-    }
-  })
+       accumulator -= 1
+    } 
+    counter ++ 
+    return accumulator < 0;
+  }) 
+  console.time('santa-time');
+  console.log('basement entered at: ', counter);
  })
-}
+}  
+
+question2() 
 
 
 
